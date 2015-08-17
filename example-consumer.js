bb5d96b5ada3821f663d249a538178391fc3b641
Kristo Kaiv <kristo.kaiv@gmail.com>
2015-08-18 00:01:18 +0300

    refactored code a bit, added automatic set up of logutriga on tables

--- example-consumer.js
+++ example-consumer.js
@@ -3,25 +3,35 @@ var pgq = require('./index.js'),
 		consumerName: 'example_consumer',
 		source: {
 			database: 'postgres://dev:dev@localhost:5432/wtaibu_monitooring',
-			queue: 'playlist.change'
+			queue: 'table.changes'
 		},
 		logDebug: true
-	};
+	},
+	consumer;
 
-var consumer = new pgq.Consumer(config);
+pgq
+.setUp(config.source.database)
+.watchTables('my_table', config.source.queue, function(err) {
+	if (err) {
+		console.log('failed to set up watched tables ', err);
+		process.exit(1);
+	}
 
-consumer.connect();
+	consumer = new pgq.Consumer(config);
+	consumer.connect();
 
-consumer.on('event', function(ev) {
-	console.log('received event with id :'+ev.id);
-	ev.tagDone();
-});
+	consumer.on('event', function(ev) {
+		console.log('received event with id :'+ev.id);
+		ev.tagDone();
+	});
 
-consumer.on('error', function(err) {
-	console.log('got error');
-	console.log(err);
-});
+	consumer.on('error', function(err) {
+		console.log('got error');
+		console.log(err);
+	});
 
-consumer.on('connect', function() {
-	console.log('consumer connected');
+	consumer.on('connect', function() {
+		console.log('consumer connected');
+	});
+
 });
